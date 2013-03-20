4db7511f8198a277abdabf726be7be0a1213223e
Winston Lin <winstonnlin@gmail.com>
2013-03-19 22:36:15 -0400

    Added Must Click on Avatar being advancing

--- MedicineReminder/src/com/example/medicinereminder/Avatar.java
+++ MedicineReminder/src/com/example/medicinereminder/Avatar.java
@@ -3,6 +3,7 @@ package com.example.medicinereminder;
 import android.os.Bundle;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
@@ -10,6 +11,7 @@ import android.view.View;
 
 public class Avatar extends Activity {
 	public static final int Tutorial_ID = 1;
+	private int imagenumber = 0;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -23,9 +25,50 @@ public class Avatar extends Activity {
 		return true;
 	}
 	
+	public void onAvatarImage1ButtonClick(View view){
+		imagenumber = 1;
+	}
+	
+	public void onAvatarImage2ButtonClick(View view){
+		imagenumber = 2;
+	}
+	
+	public void onAvatarImage3ButtonClick(View view){
+		imagenumber = 3;
+	}
+	
+	public void onAvatarImage4ButtonClick(View view){
+		imagenumber = 4;
+	}
+	
+	public void onAvatarImage5ButtonClick(View view){
+		imagenumber = 5;
+	}
+	
+	public void onAvatarImage6ButtonClick(View view){
+		imagenumber = 6;
+	}
+	
+	public void onAvatarImage7ButtonClick(View view){
+		imagenumber = 7;
+	}
+	
+	public void onAvatarImage8ButtonClick(View view){
+		imagenumber = 8;
+	}
+	
+	public void onAvatarImage9ButtonClick(View view){
+		imagenumber = 9;
+	}
+	
 	public void onAvatarContinueButtonClick(View view){
+		if (imagenumber == 0){
+			new AlertDialog.Builder(this).setTitle("Error").setMessage("Please select an avatar").setNeutralButton("close",null).show();
+		}
+		else{
 		Intent i = new Intent(this, Tutorial.class);
 		startActivityForResult(i, Tutorial_ID);
+		}
 		
 		
 	}
