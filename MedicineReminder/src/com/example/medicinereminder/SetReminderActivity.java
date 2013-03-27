df71a19ad42bc125c3cc550b5a2a61958ec53f5d
Chris Chike <cchike@seas.upenn.edu>
2013-03-27 00:26:24 -0400

    SetReminder Vaildation

--- MedicineReminder/src/com/example/medicinereminder/SetReminderActivity.java
+++ MedicineReminder/src/com/example/medicinereminder/SetReminderActivity.java
@@ -2,6 +2,7 @@ package com.example.medicinereminder;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.util.Log;
 import android.view.Menu;
@@ -31,8 +32,13 @@ public class SetReminderActivity extends Activity {
 		mes = e1.getText().toString();
 		Log.i("Info", mes);
 		
-		Intent i = new Intent(this, AppointmentsActivity.class);
-		startActivityForResult(i, Appointments_ID);
+		if (mes.equals("")){
+			new AlertDialog.Builder(this).setTitle("Error").setMessage("Please enter a reminder message").setNeutralButton("close",null).show();
+		}
+		else{
+			Intent i = new Intent(this, AppointmentsActivity.class);
+			startActivityForResult(i, Appointments_ID);
+		}
 	}
 
 }
