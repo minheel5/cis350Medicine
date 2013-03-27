cc0e233456cf3b4ddcdd9c276c5cf4b047019f9f
Chris Chike <cchike@seas.upenn.edu>
2013-03-26 20:38:30 -0400

    Updated Medication

--- MedicineReminder/src/com/example/medicinereminder/MedicationActivity.java
+++ MedicineReminder/src/com/example/medicinereminder/MedicationActivity.java
@@ -10,7 +10,7 @@ import android.widget.EditText;
 
 public class MedicationActivity extends Activity {
 	public static final int ReminderTimeActivity_ID = 1;
-	private String[] medication = {"",""};
+	private String[] medication = {"","","","",""};
 	private String[] time = {"",""};
 	
 	@Override
@@ -27,21 +27,26 @@ public class MedicationActivity extends Activity {
 	}
 	
 	public void onMContinueButtonClick(View view){
-		EditText e1 = (EditText)findViewById(R.id.editText3);
+		EditText e1 = (EditText)findViewById(R.id.editText1);
 		medication[0] = e1.getText().toString();
 		Log.i("Info", medication[0]);
 		
-		EditText e2 = (EditText)findViewById(R.id.editText4);
+		EditText e2 = (EditText)findViewById(R.id.editText2);
 		medication[1] = e2.getText().toString();
 		Log.i("Info", medication[1]);
 		
-		EditText e3 = (EditText)findViewById(R.id.editText1);
-		time[0] = e3.getText().toString();
-		Log.i("Info", time[0]);
+		EditText e3 = (EditText)findViewById(R.id.editText3);
+		medication[2] = e3.getText().toString();
+		Log.i("Info", medication[2]);
 		
-		EditText e4 = (EditText)findViewById(R.id.editText2);
-		time[1] = e4.getText().toString();
-		Log.i("Info", time[1]);
+		EditText e4 = (EditText)findViewById(R.id.editText4);
+		medication[3] = e4.getText().toString();
+		Log.i("Info", medication[3]);
+		
+		EditText e5 = (EditText)findViewById(R.id.editText5);
+		medication[4] = e5.getText().toString();
+		Log.i("Info", medication[4]);
+		
 		
 		Intent i = new Intent(this, ReminderTimeActivity.class);
 		startActivityForResult(i, ReminderTimeActivity_ID);
